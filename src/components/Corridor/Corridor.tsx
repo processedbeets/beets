5c1a2f17c65c95cf8bdc479bf2243f339939a092
David Anderson <David@beets.design>
2020-10-29 16:35:04 +0000

    add ngrx & redux logos

--- src/components/Corridor/Corridor.tsx
+++ src/components/Corridor/Corridor.tsx
@@ -5,7 +5,9 @@ import RoomContent, { HangPosition } from '../RoomContent/RoomContent';
 import Door from '../Door/Door';
 import React from 'react';
 import angular from '../../images/angular.svg';
+import ngrx from '../../images/ngrx.svg';
 import react from '../../images/react.svg';
+import redux from '../../images/redux.svg';
 import rxjs from '../../images/rxjs.svg';
 import sass from '../../images/sass.svg';
 import typescript from '../../images/typescript.svg';
@@ -35,17 +37,26 @@ const Corridor = () => {
         <RoomContent title="React" position="45" hang={HangPosition.LEFT}>
           <img src={react} alt="three blue eliptical circles with dot in centre" />
         </RoomContent>
-        <RoomContent title="Angular" position="40" hang={HangPosition.RIGHT}>
+        <RoomContent title="Redux" position="40" hang={HangPosition.RIGHT}>
+          <img src={redux} alt="Purple celtic knot shape" />
+        </RoomContent>
+        <RoomContent title="Angular" position="35" hang={HangPosition.LEFT}>
           <img src={angular} alt="red shield with letter A in the centre" />
         </RoomContent>
-        <RoomContent title="Typescript" position="35" hang={HangPosition.LEFT}>
+        <RoomContent title="Typescript" position="30" hang={HangPosition.RIGHT}>
           <img src={typescript} alt="a square with the letters T S in the corner" />
         </RoomContent>
-        <RoomContent title="Sass" position="30" hang={HangPosition.RIGHT}>
+        <RoomContent title="Sass" position="25" hang={HangPosition.LEFT}>
           <img src={sass} alt="The word 'sass' in a serif font" />
         </RoomContent>
-        <RoomContent title="RxJs" position="25" hang={HangPosition.LEFT}>
+        <RoomContent title="RxJs" position="20" hang={HangPosition.RIGHT}>
           <img src={rxjs} alt="A purple dragon circling back on itself forming a complete circle" />
+        </RoomContent>
+        <RoomContent title="NgRx" position="15" hang={HangPosition.LEFT}>
+          <img
+            src={ngrx}
+            alt="A purple dragon circling back on itself in front of a black shield"
+          />
         </RoomContent>
         <Door title="Education" position="0" />
         <Door title="Employment" position="-50" />
